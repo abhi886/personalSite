c1125f8ab5799adf1d73114df88f79a9aeffb579
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-04-16 14:06:28 +1000

    Navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../Abhishekh.png";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
@@ -8,16 +9,27 @@ function Navbar() {
 
         <a className="navbar-brand" href="#"><img className="logo" src= {logo} alt="Abhishek" /></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+        <FontAwesomeIcon icon={faBars} style = {{ color: "#fff" }}/>
   </button>
-
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav ml-auto">
+      <ul className="navbar-nav ml-auto">
       <li className="nav-item active">
         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
+        <a className="nav-link" href="#">About Me</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Services</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Working Experience</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Portfolio</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Contacts</a>
       </li>
     </ul>
   </div>
