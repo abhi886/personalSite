2fbfa2a43d205ca004251df08091a7fcd377f8f4
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-06-29 20:29:13 +1000

    Create blog component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,52 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import Particles from 'react-particles-js';
-import AboutMe from "./components/AboutMe";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Particles from "react-particles-js";
+import Blog from "./components/Blog";
+// import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
-import Experience from "./components/Experience";
-import Portfolio from "./components/Portfolio";
-import Testimonials from "./components/Testimonials";
-import Contacts from "./components/Contacts";
+// import Experience from "./components/Experience";
+// import Portfolio from "./components/Portfolio";
+// import Testimonials from "./components/Testimonials";
+// import Contacts from "./components/Contacts";
 import Footer from "./components/Footer";
-
-
-
 
 function App() {
   return (
     <>
-    <Particles 
-    className = 'particles-canvas'
-    parans={{
-      particles:{
-        number:{
-          value: 30,
-          density:{
-            enable: true, 
-            value_area: 900,
-          }
-        },
-        shape: {
-          type: "circle",
-          stroke: {
-            width: 6,
-            color: "#f9ab00"
-          }
-        }
-      }
-    }} />
-   <Navbar />
-   <Header />
-   <AboutMe />
-   <Services />
-   <Experience />
-   <Portfolio />
-   <Testimonials />
-   <Contacts />
-   <Footer />
-
-
-   </>
-
-
+      <Particles
+        className='particles-canvas'
+        parans={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00",
+              },
+            },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+      <Blog />
+      {/* <AboutMe /> */}
+      <Services />
+      {/*   <Experience />
+      <Portfolio />
+      <Testimonials />
+      <Contacts /> */}
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Blog.js
@@ -0,0 +1,96 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBlog } from "@fortawesome/free-solid-svg-icons";
+
+const Blog = () => {
+  return (
+    <div>
+      <div id='blog' className='blog'>
+        <div className='blog-heading container'>
+          <h2 className='py-5'>
+            {" "}
+            <FontAwesomeIcon className='icon' icon={faBlog} size='2x' /> My
+            Latest Blogs{" "}
+          </h2>
+          <div className='row blogs-content '>
+            <div className='borderbox btm-5m col-md-12'>
+              <div className='row cm10-row'>
+                <div className='col-md-5'>
+                  <div className='img-box'>
+                    <img
+                      className='blog-img'
+                      src='https://rollingnexus.com/image/cache/catalog/Blogs_JPEG_Feb/Most-common-job-interview-question-II.jpg-540x300.jpg'
+                      alt='author...'
+                    />
+                    <div className='overlay'></div>
+                  </div>
+                </div>
+                <div className='col-md-7'>
+                  <h5>You Dont Need To Remember Everythig to Code...</h5>
+                  <ul className='sub-title'>
+                    {" "}
+                    <li>
+                      <i className='fa fa-calendar' aria-hidden='true'></i> Jun
+                      25, 2021
+                    </li>{" "}
+                    <li>
+                      <i className='far fa-eye' aria-hidden='true'></i> ( About
+                      5 mins read )
+                    </li>{" "}
+                  </ul>
+
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Necessitatibus, magnam. Quasi dolore porro unde, ex at,
+                    ducimus facere cumque sed, odit dolorum aliquam et adipisci
+                    blanditiis autem incidunt. Velit, id?
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Second Blog */}
+
+          <div className='row blogs-content'>
+            <div className='borderbox btm-5m col-md-12'>
+              <div className='row cm10-row'>
+                <div className='col-md-5'>
+                  <div className='img-box'>
+                    <img
+                      className='blog-img'
+                      src='https://rollingnexus.com/image/cache/catalog/Blogs_JPEG_Feb/Most-common-job-interview-question-II.jpg-540x300.jpg'
+                      alt='author...'
+                    />
+                    <div className='overlay'></div>
+                  </div>
+                </div>
+                <div className='col-md-7'>
+                  <h5>You Dont Need To Remember Everythig to Code...</h5>
+                  <ul className='sub-title'>
+                    {" "}
+                    <li>
+                      <i className='far fa-eye'></i> 24{" "}
+                    </li>{" "}
+                    <li>
+                      <i className='far fa-clock'></i> Jun 25, 2021
+                    </li>{" "}
+                  </ul>
+
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Necessitatibus, magnam. Quasi dolore porro unde, ex at,
+                    ducimus facere cumque sed, odit dolorum aliquam et adipisci
+                    blanditiis autem incidunt. Velit, id?
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
