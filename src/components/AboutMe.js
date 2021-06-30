dedf2aa850b343e12055eddfaaa9bf855495fa9b
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-06-30 12:46:03 +1000

    Add view all button

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React from "react";
 import author from "../abhishek.jpg";
 
 const AboutMe = () => {
-    return (
-        <div id="about" className="container py-5">
-            <div className="row">
-            <div className="col-lg-6 col-xm-12">
-                <div className="photo-wrap mb-5" >
-                <img className="profile-img" src={author} alt="author..."/>
-                </div>
+  return (
+    <div id='about' className='container py-5'>
+      <div className='aboutme-container'>
+        <div className='row'>
+          <div className='col-lg-6 col-xm-12'>
+            <div className='photo-wrap mb-5'>
+              <img className='profile-img' src={author} alt='author...' />
             </div>
-            <div className="col-lg-6 col-xm-12">
-                <h1 className="about-heading">About Me</h1>
-                <p>Hi, I am Abhishekh Maharjan, 
-                    a Software Engineer with a strong background that encompasses diverse aspects of Software Engineering.
-                    Currently, I am living in Canberra, Autralia. I’m seeking to heighten my experience towards becoming a Full Stack Software Engineer. 
-                    My programming acumen includes knowledge in React, Nodejs and JavaScript ecosystem. 
-                    </p>
-            </div>
-
-            </div> 
+          </div>
+          <div className='col-lg-6 col-xm-12'>
+            <h1 className='about-heading'>About Me</h1>
+            <p>
+              Hi, I am Abhishekh Maharjan, a Software Engineer with a strong
+              background that encompasses diverse aspects of Software
+              Engineering. Currently, I am living in Canberra, Autralia. I’m
+              seeking to heighten my experience towards becoming a Full Stack
+              Software Engineer. My programming acumen includes knowledge in
+              React, Nodejs and JavaScript ecosystem.
+            </p>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBlog } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBlog,
+  faGlasses,
+  faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Blog = () => {
   return (
@@ -34,8 +39,13 @@ const Blog = () => {
                       25, 2021
                     </li>{" "}
                     <li>
-                      <i className='far fa-eye' aria-hidden='true'></i> ( About
-                      5 mins read )
+                      <FontAwesomeIcon
+                        className='icon'
+                        icon={faGlasses}
+                        size=''
+                      />
+                      {/* <i className='far fa-eye' aria-hidden='true'></i> */}(
+                      About 5 mins read )
                     </li>{" "}
                   </ul>
 
@@ -49,43 +59,12 @@ const Blog = () => {
               </div>
             </div>
           </div>
-
-          {/* Second Blog */}
-
-          <div className='row blogs-content'>
-            <div className='borderbox btm-5m col-md-12'>
-              <div className='row cm10-row'>
-                <div className='col-md-5'>
-                  <div className='img-box'>
-                    <img
-                      className='blog-img'
-                      src='https://rollingnexus.com/image/cache/catalog/Blogs_JPEG_Feb/Most-common-job-interview-question-II.jpg-540x300.jpg'
-                      alt='author...'
-                    />
-                    <div className='overlay'></div>
-                  </div>
-                </div>
-                <div className='col-md-7'>
-                  <h5>You Dont Need To Remember Everythig to Code...</h5>
-                  <ul className='sub-title'>
-                    {" "}
-                    <li>
-                      <i className='far fa-eye'></i> 24{" "}
-                    </li>{" "}
-                    <li>
-                      <i className='far fa-clock'></i> Jun 25, 2021
-                    </li>{" "}
-                  </ul>
-
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Necessitatibus, magnam. Quasi dolore porro unde, ex at,
-                    ducimus facere cumque sed, odit dolorum aliquam et adipisci
-                    blanditiis autem incidunt. Velit, id?
-                  </p>
-                </div>
-              </div>
-            </div>
+          <div className='blog-btn-viewMore'>
+            <Link to='/' className='btn-blog '>
+              {" "}
+              View All{" "}
+              <FontAwesomeIcon className='icon' icon={faArrowRight} size='' />
+            </Link>
           </div>
         </div>
       </div>
