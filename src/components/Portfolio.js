88d1c7b0eaf446156766ff9397396668af2e6653
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-07-02 21:22:52 +1000

    Portfolio changes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,72 +1,134 @@
-import React from 'react';
-import netflix from "../images/netflix.png";
+import React from "react";
+import rollingNexus from "../images/rollingNexus.png";
+import rollingPlans from "../images/rollingPlans.png";
+
 // FONT AWESOME IMPORTS
-import { FontAwesomeIcon } from  "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
 // REACT POPUPBOX
-import { PopupboxManager, PopupboxContainer} from "react-popupbox";
+import { PopupboxManager, PopupboxContainer } from "react-popupbox";
 import "react-popupbox/dist/react-popupbox.css";
 
 const Portfolio = () => {
-    //Netflix
-    const openPopupboxNetflix = () => {
-        const content = (
-        <>
-        <img className="portfolio-image-popupbox" src={netflix} alt="Netflix Clone Project"/>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem, impedit.</p>
-        <b>GitHub:</b>  <a className="hyperlink" onClick ={() => window.open("https://github.com/abhi886/hisab-Kitab")}>https://github.com/abhi886/hisab-Kitab</a>
-        </>)
-        PopupboxManager.open({ content })
-    }
+  //Rolling Nexus
+  const openPopupboxRollingNexus = () => {
+    const content = (
+      <>
+        <img
+          className='portfolio-image-popupbox'
+          src={rollingNexus}
+          alt='Rolling Nexus'
+        />
+        <p>A Professional Networking Site.</p>
+        <b>URL:</b>{" "}
+        <a
+          className='hyperlink'
+          onClick={() => window.open("https://rollingnexus.com/")}
+        >
+          https://rollingnexus.com/
+        </a>
+      </>
+    );
+    PopupboxManager.open({ content });
+  };
 
-    const popupboxConfigNetflix = {
-        titleBar: {
-            enable:true,
-            text: "Netflix clone project."
+  // Rolling Plans
+  const openPopupboxRollingPlans = () => {
+    const content = (
+      <>
+        <img
+          className='portfolio-image-popupbox'
+          src={rollingPlans}
+          alt='Rolling Plans'
+        />
+        <p>An Electronic Recruitment system</p>
+        <b>URL:</b>{" "}
+        <a
+          className='hyperlink'
+          onClick={() => window.open("https://rollingplans.com.np/")}
+        >
+          https://rollingplans.com.np/
+        </a>
+      </>
+    );
+    PopupboxManager.open({ content });
+  };
+  const popupboxConfigRollingNexus = {
+    titleBar: {
+      enable: true,
+      text: "Rolling Nexus",
+    },
+    fadeIn: true,
+    fadeInSpeed: 500,
+  };
+  const popupboxConfigRollingplans = {
+    titleBar: {
+      enable: true,
+      text: "Rolling Plans",
+    },
+    fadeIn: true,
+    fadeInSpeed: 500,
+  };
+  return (
+    <>
+      <div id='portfolio' className='portfolio-wraper'>
+        <div className='container'>
+          <h1 className='text-uppercase text-center py-5'>Portfolio</h1>
+          <div className='image-box-wrapper row justify-content-center'>
+            <div
+              className='portfolio-image-box'
+              onClick={openPopupboxRollingNexus}
+            >
+              <img
+                className='portfolio-image'
+                src={rollingNexus}
+                alt='Rolling Nexus Project'
+              />
+              <div className='overflow'></div>
+              <FontAwesomeIcon className='portfolio-icon' icon={faSearchPlus} />
+            </div>
 
-        },
-        fadeIn: true,
-        fadeInSpeed: 500
-    }
-    return (
-       <div id="portfolio" className="portfolio-wraper">
-           <div className="container">
-               <h1 className="text-uppercase text-center py-5">Portfolio</h1>
-               <div className="image-box-wrapper row justify-content-center">
-                   <div className="portfolio-image-box" onClick = {openPopupboxNetflix}>
-                   <img className="portfolio-image" src={netflix} alt="Netflix clone project"/>
-                   <div className="overflow"></div>
-                   <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-                   </div>
+            {/* - */}
+            <div
+              className='portfolio-image-box'
+              onClick={openPopupboxRollingPlans}
+            >
+              <img
+                className='portfolio-image'
+                src={rollingPlans}
+                alt='Rolling Plans Project'
+              />
+              <div className='overflow'></div>
+              <FontAwesomeIcon className='portfolio-icon' icon={faSearchPlus} />
+            </div>
+            {/* - */}
+            {/* <div className='portfolio-image-box'>
+            <img
+              className='portfolio-image'
+              src={netflix}
+              alt='Netflix clone project'
+            />
+            <div className='overflow'></div>
+            <FontAwesomeIcon className='portfolio-icon' icon={faSearchPlus} />
+          </div> */}
+            {/* - */}
+            {/* <div className='portfolio-image-box'>
+            <img
+              className='portfolio-image'
+              src={netflix}
+              alt='Netflix clone project'
+            />
+            <div className='overflow'></div>
+            <FontAwesomeIcon className='portfolio-icon' icon={faSearchPlus} />
+          </div> */}
+            {/* - */}
+          </div>
+        </div>
+        <PopupboxContainer {...popupboxConfigRollingNexus} />{" "}
+        <PopupboxContainer {...popupboxConfigRollingplans} />
+      </div>
+    </>
+  );
+};
 
-               {/* - */}
-               <div className="portfolio-image-box">
-                   <img className="portfolio-image" src={netflix} alt="Netflix clone project"/>
-                   <div className="overflow"></div>
-                   <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-
-                   </div>
-                                  {/* - */}
-               <div className="portfolio-image-box">
-                   <img className="portfolio-image" src={netflix} alt="Netflix clone project"/>
-                   <div className="overflow"></div>
-                   <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-
-                   </div>
-                                  {/* - */}
-               <div className="portfolio-image-box">
-                   <img className="portfolio-image" src={netflix} alt="Netflix clone project"/>
-                   <div className="overflow"></div>
-                   <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-
-                   </div>
-                                  {/* - */}
-                                  </div>
-           </div>
-           <PopupboxContainer {...popupboxConfigNetflix} />
-       </div>
-
-    )
-}
-
-export default Portfolio
+export default Portfolio;
