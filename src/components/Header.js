4e6c12315663c8ef069ed83993611a196bfaaaa1
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-07-28 13:52:21 +1000

    Add Social Button in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,55 @@
 import React from "react";
 import Typed from "react-typed";
+import { Link } from "react-router-dom";
+
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faFacebookF, faBlog } from "@fortawesome/free-solid-svg-icons";
+// import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
+
+import { FaFacebookF, FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Header = () => {
-    return (
-        <div id="home" className="header-wraper">
-            <div className="main-info">
-                <h1>HI. I AM ABHISHEKH MAHARJAN</h1>
-                <Typed
-                className="typed-text"
-                strings={["Software Engineer","Full Stack Software Developer", "Web Developer"]}
-                typeSpeed={20}
-                backSpeed={40}
-                loop
-                />
-                <a href="#" className="btn-main-offer"> CONTACT ME !</a>
-            </div>
-            
+  return (
+    <div id='home' className='header-wraper'>
+      <div className='main-info'>
+        <h1>HI. I AM ABHISHEKH MAHARJAN</h1>
+        <Typed
+          className='typed-text'
+          strings={[
+            "Software Engineer",
+            "Full Stack Software Developer",
+            "Web Developer",
+          ]}
+          typeSpeed={20}
+          backSpeed={40}
+          loop
+        />
+        <Link to='#' className='btn-main-offer'>
+          {" "}
+          CONTACT ME !
+        </Link>
+        <div className='social'>
+          <ul className=''>
+            <li>
+              <Link to='/#'>
+                <FaGithub size={30} />
+              </Link>
+            </li>
+            <li>
+              <Link to='/#'>
+                <FaLinkedinIn size={30} />
+              </Link>
+            </li>
+            <li>
+              <Link to='/#'>
+                <FaFacebookF size={30} />
+              </Link>
+            </li>
+          </ul>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
