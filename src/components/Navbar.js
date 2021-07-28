66f5f0b974764ffc205fb137dfd0c918f178bac7
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-07-29 09:52:37 +1000

    Change NavBar to class component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,70 +1,73 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink, Link } from "react-router-dom";
 import logo from "../Abhishekh.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 // import { Link } from "react-scroll";
 
-function Navbar() {
-  return (
-    <nav className='navbar navbar-expand-lg navbar-light bg-dark fixed-top'>
-      <div className='container'>
-        <Link className='navbar-brand' to='/'>
-          <img className='logo' src={logo} alt='Abhishek' />
-        </Link>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-toggle='collapse'
-          data-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
-        </button>
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul className='navbar-nav ml-auto'>
-            <li className='nav-item'>
-              <NavLink exact to='/' className='nav-link'>
-                Home <span className='sr-only'>(current)</span>
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink to='/about' className='nav-link'>
-                About Me
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink smooth='true' to='/services' className='nav-link'>
-                Services
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink smooth='true' to='/experience' className='nav-link'>
-                Working Experience
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink smooth='true' to='/portfolio' className='nav-link'>
-                Portfolio
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink smooth='true' to='/contacts' className='nav-link'>
-                Contacts
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink smooth='true' to='/login' className='nav-link'>
-                Login
-              </NavLink>
-            </li>
-          </ul>
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <nav className='navbar navbar-expand-lg navbar-light bg-dark fixed-top'>
+        <div className='container'>
+          <Link className='navbar-brand' to='/'>
+            <img className='logo' src={logo} alt='Abhishek' />
+          </Link>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbarSupportedContent'
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
+          </button>
+          <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+            <ul className='navbar-nav ml-auto'>
+              <li className='nav-item' onclick={this.handleClick}>
+                <NavLink exact to='/' className='nav-link'>
+                  Home <span className='sr-only'>(current)</span>
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink to='/about' className='nav-link'>
+                  About Me
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink smooth='true' to='/services' className='nav-link'>
+                  Services
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink smooth='true' to='/experience' className='nav-link'>
+                  Working Experience
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink smooth='true' to='/portfolio' className='nav-link'>
+                  Portfolio
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink smooth='true' to='/contacts' className='nav-link'>
+                  Contacts
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink smooth='true' to='/login' className='nav-link'>
+                  Login
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  );
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
