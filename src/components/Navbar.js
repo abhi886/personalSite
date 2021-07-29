0d0f4fc63e36f8a7903e313b3e79f46c535708c2
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-07-29 11:06:51 +1000

    Fix navbar toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,9 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 // import { Link } from "react-scroll";
 
 class Navbar extends Component {
+  handleSelect = () => {
+    console.log("Clicked");
+  };
   state = {};
   render() {
     return (
@@ -27,37 +30,65 @@ class Navbar extends Component {
           </button>
           <div className='collapse navbar-collapse' id='navbarSupportedContent'>
             <ul className='navbar-nav ml-auto'>
-              <li className='nav-item' onclick={this.handleClick}>
+              <li
+                className='nav-item'
+                data-toggle='collapse'
+                data-target='#navbarSupportedContent'
+              >
                 <NavLink exact to='/' className='nav-link'>
                   Home <span className='sr-only'>(current)</span>
                 </NavLink>
               </li>
-              <li className='nav-item'>
+              <li
+                className='nav-item'
+                data-toggle='collapse'
+                data-target='#navbarSupportedContent'
+              >
                 <NavLink to='/about' className='nav-link'>
                   About Me
                 </NavLink>
               </li>
-              <li className='nav-item'>
+              <li
+                className='nav-item'
+                data-toggle='collapse'
+                data-target='#navbarSupportedContent'
+              >
                 <NavLink smooth='true' to='/services' className='nav-link'>
                   Services
                 </NavLink>
               </li>
-              <li className='nav-item'>
+              <li
+                className='nav-item'
+                data-toggle='collapse'
+                data-target='#navbarSupportedContent'
+              >
                 <NavLink smooth='true' to='/experience' className='nav-link'>
                   Working Experience
                 </NavLink>
               </li>
-              <li className='nav-item'>
+              <li
+                className='nav-item'
+                data-toggle='collapse'
+                data-target='#navbarSupportedContent'
+              >
                 <NavLink smooth='true' to='/portfolio' className='nav-link'>
                   Portfolio
                 </NavLink>
               </li>
-              <li className='nav-item'>
+              <li
+                className='nav-item'
+                data-toggle='collapse'
+                data-target='#navbarSupportedContent'
+              >
                 <NavLink smooth='true' to='/contacts' className='nav-link'>
                   Contacts
                 </NavLink>
               </li>
-              <li className='nav-item'>
+              <li
+                className='nav-item'
+                data-toggle='collapse'
+                data-target='#navbarSupportedContent'
+              >
                 <NavLink smooth='true' to='/login' className='nav-link'>
                   Login
                 </NavLink>
