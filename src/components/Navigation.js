8a13b57d42000579591d525f93cf26ced20b70b2
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-08-06 10:18:11 +1000

    Added CDN for bootstrap js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,9 +10,11 @@ import Container from "react-bootstrap/Container"; // import { NavLink, Link } f
 
 const Navigation = () => {
   return (
-    <Navbar bg='light' expand='lg'>
+    <Navbar bg='dark' expand='lg'>
       <Container>
-        <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href='#home'>
+          <Image src='holder.js/171x180' rounded />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'></Navbar.Collapse>
       </Container>
