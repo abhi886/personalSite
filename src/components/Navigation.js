2f0e417e5705fb310ff11bc876154f6e1c7c7023
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-08-06 12:52:27 +1000

    NavBar: Completed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,26 +3,29 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Image from "react-bootstrap/Image";
 import logo from "../images/Abhishekh.png";
-// import Nav from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
-import Container from "react-bootstrap/Container"; // import { NavLink, Link } from "react-router-dom";
-// import logo from "../Abhishekh.png";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faBars } from "@fortawesome/free-solid-svg-icons";
-// import { Link } from "react-scroll";
+import Container from "react-bootstrap/Container";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navigation = () => {
   return (
-    <Navbar collapseOnSelect bg='dark' expand='sm'>
+    <Navbar collapseOnSelect bg='dark' expand='lg'>
       <Container>
         <Navbar.Brand href='#home'>
           <Image src={logo} className='logo' />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-        <Navbar.Collapse id='responsive-navbar-nav'>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav'>
+          {" "}
+          <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
+        </Navbar.Toggle>
+        <Navbar.Collapse
+          id='responsive-navbar-nav'
+          className='justify-content-end'
+        >
           <Nav className='me-auto'>
-            <Nav.Link href='#features'>Home</Nav.Link>
-            <Nav.Link href='#pricing'>About Me</Nav.Link>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#about-me'>About Me</Nav.Link>
             <Nav.Link href='#services'>Services</Nav.Link>
             <Nav.Link href='#Experiene'>Experience</Nav.Link>
             <Nav.Link href='#portfolio'>Portfolio</Nav.Link>
